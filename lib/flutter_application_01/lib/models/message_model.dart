775c6f7755939ef35081ee1094043d84e27318e8
Aki Sumire <akisumire610@gmail.com>
2021-10-16 19:11:10 +0900

    tutorial
    練習用

---
+++ lib/flutter_application_01/lib/models/message_model.dart
@@ -0,0 +1,63 @@
+import 'package:flutter_application_2/models/user_model.dart';
+
+class Message {
+  final User sender;
+  final String time;
+  final String text;
+  final bool isLiked;
+  final bool unread;
+
+  Message({
+    this.sender,
+    this.time,
+    this.text,
+    this.isLiked,
+    this.unread,
+  });
+}
+
+// you -current user
+final User currentUser = User(
+  id: 0,
+  name: 'Current User',
+  imageUrl: 'assets/images/greg.jpg',
+);
+
+// Users
+final User greg = User(
+  id: 1,
+  name: 'Greg',
+  imageUrl: 'assets/images/greg.jpg',
+);
+final User james = User(
+  id: 2,
+  name: 'James',
+  imageUrl: 'assets/images/james.jpg',
+);
+final User oliva = User(
+  id: 3,
+  name: 'Oliva',
+  imageUrl: 'assets/images/oliva.jpg',
+);
+
+// FAVORITE CONTACTS
+List<User> favorites = [james, oliva];
+
+// EXAMPLE CHATS ON HOME SCREEN
+List<Message> chats = [
+  Message(
+    sender: james,
+    time: '5:30 PM',
+    text: 'Hey, how\'s it going? What did you do today?',
+    isLiked: false,
+    unread: true,
+  ),
+  Message(
+    sender: oliva,
+    time: '4:30 PM',
+    text: 'Hey, how\'s it going? What did you do today?',
+    isLiked: false,
+    unread: true,
+  )
+];
+// EXAMPLE MESSAGE IN CHAT SCREEN
